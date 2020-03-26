991437d33273553064dc205edfa624403a8d3b10
shaisxx <shaisxx@users.noreply.github.com>
2020-03-26 10:19:59 +0800

    修改微信小程序的配置信息

--- utils/config.js
+++ utils/config.js
@@ -1,8 +1,8 @@
-var API_BASE = 'https://dogstar.api.yesapi.cn';
+var API_BASE = 'https://hn216.api.yesapi.cn';
 
 const CONFIG = {
     API_URL: {
-      GET_INDEX: API_BASE + '?app_key=CEE4B8A091578B252AC4C92FB4E893C3',
+      GET_INDEX: API_BASE + '?app_key=731B85BC7F350326BC27BA1652577EBD',
         GET_PAGE: API_BASE + '&page_id=',
         GET_ARTICLE: API_BASE + '&p=',
         GET_CATEGORY: API_BASE + '&cat=',
